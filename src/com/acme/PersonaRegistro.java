c86fe2c2216f37091802aba321aec9c445adc5fd
ejenkinsh <edwjenkher@gmail.com>
2024-01-05 13:03:45 -0600

    Se quito el ui designer y puso comentario

--- src/com/acme/PersonaRegistro.java
+++ src/com/acme/PersonaRegistro.java
@@ -5,7 +5,7 @@ public record PersonaRegistro(String nombre, String apellidos) {
     @Override
     public String toString() {
         return "%s $s".formatted(nombre, apellidos);
-
+        //comentario
 
     }
 }
